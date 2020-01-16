c03bfddb4b3baa1251b6495dcc65f456ead6c00b
Christoph Amrein <camrein@hsr.ch>
2020-01-16 09:28:12 +0100

    Added integration test for resources that can be disposed asynchronously.

---
+++ test/IntegrationTests/IntegrationTests.Cli/FileReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IntegrationTests.Cli {
+  public class FileReader {
+    public async IAsyncEnumerable<byte[]> GetFileContents(string fileName, int chunkSize) {
+      using var input = File.OpenRead(fileName);
+      int len;
+      var buffer = new byte[chunkSize];
+      while((len = await input.ReadAsync(buffer)) != -1) {
+        var output = new byte[len];
+        Array.Copy(buffer, 0, output, 0, len);
+        yield return buffer;
+      }
+    }
+  }
+}
