b75eda6d0593439477d4cd5f9cda6a0ff9f56ee9
Christoph Amrein <camrein@hsr.ch>
2020-02-11 13:19:09 +0100

    Added unit tests for the linq extensions.

---
+++ src/ParallelHelper.Test/Extensions/LinqExtensionsTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ParallelHelper.Test.Extensions {
+  [TestClass]
+  public class LinqExtensionsTest {
+    [TestMethod, ExpectedException(typeof(OperationCanceledException))]
+    public void WithCancellationThrowsOperationCanceledExceptionWhenCancellationIsRequested() {
+      var canceledToken = new CancellationTokenSource();
+      canceledToken.Cancel();
+      Enumerable.Range(0, 10)
+        .WithCancellation(canceledToken.Token)
+        .ToArray();
+      Assert.Fail("cancellation token not respected");
+    }
+
+    [TestMethod]
+    public void WithCancellationDoesNotThrowOperationCanceledExceptionWhenNoCancellationIsRequested() {
+      var canceledToken = new CancellationTokenSource();
+      var array = Enumerable.Range(0, 10)
+        .WithCancellation(canceledToken.Token)
+        .ToArray();
+      Assert.AreEqual(10, array.Length);
+    }
+
+    [TestMethod]
+    public void IsNotNullFiltersAllNullValues() {
+      var input = new List<string?> {
+        "a", "b", null, "c", null, "d", null
+      };
+      var output = input.IsNotNull().ToList();
+      var expected = new List<string> {
+        "a", "b", "c", "d"
+      };
+      CollectionAssert.AreEqual(expected, output);
+    }
+  }
+}
