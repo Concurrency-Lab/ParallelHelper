ac18420b2eb56fec69b6372bde57fd112912d951
Christoph Amrein <camrein@hsr.ch>
2020-02-13 08:33:03 +0100

    Added unit tests for the help link factory.

---
+++ src/ParallelHelper.Test/Util/HelpLinkFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Util;
+
+namespace ParallelHelper.Test.Util {
+  [TestClass]
+  public class HelpLinkFactoryTest {
+    [TestMethod]
+    public void CreatesGitHubUrlWithGivenDiagnosticId() {
+      Assert.AreEqual(
+        "https://github.com/Concurrency-Lab/ParallelHelper/tree/master/doc/analyzers/PH_B001.md",
+        HelpLinkFactory.CreateUri("PH_B001")
+      );
+      Assert.AreEqual(
+        "https://github.com/Concurrency-Lab/ParallelHelper/tree/master/doc/analyzers/PH_S013.md",
+        HelpLinkFactory.CreateUri("PH_S013")
+      );
+    }
+  }
+}
