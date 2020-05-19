7daaf6ba73a1f7a750531c52bd179c5005ffc395
Christoph Amrein <camrein@users.noreply.github.com>
2020-05-19 09:26:43 +0200

    Added analyzer for the use of ThreadLocal inside async methods.

---
+++ src/ParallelHelper.Test/Analyzer/Smells/ThreadLocalInAsynchronousMethodAnalyzerTest.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.Smells;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
+  public class ThreadLocalInAsynchronousMethodAnalyzerTest : AnalyzerTestBase<ThreadLocalInAsynchronousMethodAnalyzer> {
+    [TestMethod]
+    public void ReportsThreadLocalReadInAsyncMethod() {
+      const string source = @"
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public async Task<int> GetCountAsync() {
+    await Task.Delay(100);
+    return count.Value;
+  }
+}";
+      VerifyDiagnostic(source, new DiagnosticResultLocation(9, 12));
+    }
+
+    [TestMethod]
+    public void ReportsThreadLocalWriteInAsyncDelegate() {
+      const string source = @"
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public void DoWork() {
+    Func<Task> reset = async delegate {
+      await Task.Delay(100);
+      count.Value = 0;
+    };
+  }
+}";
+      VerifyDiagnostic(source, new DiagnosticResultLocation(11, 7));
+    }
+
+    [TestMethod]
+    public void ReportsThreadLocalReadWriteInAsyncParenthesizedLambda() {
+      const string source = @"
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public void DoWork() {
+    Func<int, Task<int>> getAndSet = async (newValue) => {
+      var oldValue = count.Value;
+      await Task.Delay(100);
+      count.Value = newValue;
+      return oldValue;
+    };
+  }
+}";
+      VerifyDiagnostic(source, new DiagnosticResultLocation(10, 22), new DiagnosticResultLocation(12, 7));
+    }
+
+    [TestMethod]
+    public void ReportsThreadLocalReadInAsyncLambda() {
+      const string source = @"
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public void DoWork() {
+    Func<Task<int>> action = async () => count.Value;
+  }
+}";
+      VerifyDiagnostic(source, new DiagnosticResultLocation(9, 42));
+    }
+
+    [TestMethod]
+    public void ReportsThreadLocalWriteInAsyncLocalFunctionInMethod() {
+      const string source = @"
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public void DoWork() {
+    async Task Reset() => count.Value = 0;
+  }
+}";
+      VerifyDiagnostic(source, new DiagnosticResultLocation(8, 27));
+    }
+
+    [TestMethod]
+    public void DoesNotReportThreadLocalReadInNonAsyncMethod() {
+      const string source = @"
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public int GetValue() {
+    return count.Value;
+  }
+}";
+      VerifyDiagnostic(source);
+    }
+
+    [TestMethod]
+    public void DoesNotReportLocalWriteInLambdaInAsyncMethod() {
+      const string source = @"
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public async Task DoWork() {
+    Action reset = () => count.Value = 0;
+  }
+}";
+      VerifyDiagnostic(source);
+    }
+
+    [TestMethod]
+    public void DoesNotReportThreadLocalReadInLocalFunctionInAsyncMethod() {
+      const string source = @"
+using System.Threading;
+using System.Threading.Tasks;
+
+class Test {
+  private readonly ThreadLocal<int> count = new ThreadLocal<int>();
+
+  public async Task DoWork() {
+    int Count() => count.Value;
+  }
+}";
+      VerifyDiagnostic(source);
+    }
+  }
+}
---
+++ src/ParallelHelper/Analyzer/Smells/ThreadLocalInAsynchronousMethodAnalyzer.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using ParallelHelper.Extensions;
+using ParallelHelper.Util;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace ParallelHelper.Analyzer.Smells {
+  /// <summary>
+  /// Analyzer that analyzes sources for the use of <see cref="System.Threading.ThreadLocal{T}"/> instances inside asynchronous methods.
+  /// 
+  /// <example>Illustrates a class with an asynchronous method that reads and writes a thread local value.
+  /// <code>
+  /// class Sample {
+  ///   private readonly ThreadLocal&lt;int&gt; count = new ThreadLocal&lt;int&gt;();
+  /// 
+  ///   public async Task&lt;int&gt; GetAndIncrementAsync() {
+  ///     count.Value++;
+  ///     await Task.Delay(100);
+  ///     return count.Value;
+  ///   }
+  /// }
+  /// </code>
+  /// </example>
+  /// </summary>
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class ThreadLocalInAsynchronousMethodAnalyzer : DiagnosticAnalyzer {
+    public const string DiagnosticId = "PH_S029";
+
+    private const string Category = "Concurrency";
+
+    private static readonly LocalizableString Title = "ThreadLocal in Async Method";
+    private static readonly LocalizableString MessageFormat = "The use of ThreadLocal inside asynchronous methods is discouraged. Consider the use of AsyncLocal instead.";
+    private static readonly LocalizableString Description = "";
+
+    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+      DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning,
+      isEnabledByDefault: true, description: Description, helpLinkUri: HelpLinkFactory.CreateUri(DiagnosticId)
+    );
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    private const string ThreadLocalType = "System.Threading.ThreadLocal`1";
+    private const string ValueProperty = "Value";
+
+    public override void Initialize(AnalysisContext context) {
+      context.EnableConcurrentExecution();
+      context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+      context.RegisterSyntaxNodeAction(
+        AnalyzeMethodDeclaration,
+        SyntaxKind.MethodDeclaration,
+        SyntaxKind.SimpleLambdaExpression,
+        SyntaxKind.ParenthesizedLambdaExpression,
+        SyntaxKind.AnonymousMethodExpression,
+        SyntaxKind.LocalFunctionStatement
+      );
+    }
+
+    private static void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context) {
+      new Analyzer(context).Analyze();
+    }
+
+    private class Analyzer : SyntaxNodeAnalyzerBase<SyntaxNode> {
+      public Analyzer(SyntaxNodeAnalysisContext context) : base(context) { }
+
+      public override void Analyze() {
+        if(!Node.IsMethodOrFunctionWithAsyncModifier()) {
+          return;
+        }
+        foreach(var memberAccess in GetThreadLocalValueMemberAccesses()) {
+          Context.ReportDiagnostic(Diagnostic.Create(Rule, memberAccess.GetLocation()));
+        }
+      }
+
+      private IEnumerable<MemberAccessExpressionSyntax> GetThreadLocalValueMemberAccesses() {
+        return Node.DescendantNodesInSameActivationFrame()
+          .WithCancellation(CancellationToken)
+          .OfType<MemberAccessExpressionSyntax>()
+          .Where(IsThreadLocalValueProperty);
+      }
+
+      private bool IsThreadLocalValueProperty(MemberAccessExpressionSyntax memberAccess) {
+        return SemanticModel.GetSymbolInfo(memberAccess, CancellationToken).Symbol is IPropertySymbol property
+          && ValueProperty.Equals(property.Name)
+          && SemanticModel.IsEqualType(property.ContainingType, ThreadLocalType);
+      }
+    }
+  }
+}
