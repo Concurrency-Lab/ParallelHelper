47b1f1202fbd1611114757c353bf4e2c7a54376a
Christoph Amrein <camrein@users.noreply.github.com>
2020-08-05 13:47:33 +0200

    Added analyzer to detect invocations of async void methods.

---
+++ src/ParallelHelper.Test/Analyzer/Smells/AsyncVoidMethodInvocationAnalyzerTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.Smells;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
+  public class AsyncVoidMethodInvocationAnalyzerTest : AnalyzerTestBase<AsyncVoidMethodInvocationAnalyzer> {
+    [TestMethod]
+    public void ReportsInvocationOfAsyncVoidMethod() {
+      const string source = @"
+using System.Threading.Tasks;
+
+class Test {
+  public void DoIt() {
+    DoItAsync();
+  }
+
+  public async void DoItAsync() {}
+}";
+      VerifyDiagnostic(source, new DiagnosticResultLocation(5, 5));
+    }
+
+    [TestMethod]
+    public void DoesNotReportInvocationOfAsyncTaskMethod() {
+      const string source = @"
+using System.Threading.Tasks;
+
+class Test {
+  public void DoIt() {
+    DoItAsync();
+  }
+
+  public async Task DoItAsync() {}
+}";
+      VerifyDiagnostic(source);
+    }
+
+    [TestMethod]
+    public void DoesNotReportInvocationOfTaskMethod() {
+      const string source = @"
+using System.Threading.Tasks;
+
+class Test {
+  public void DoIt() {
+    DoItAsync();
+  }
+
+  public Task DoItAsync() {
+    return Task.CompletedTask;
+  }
+}";
+      VerifyDiagnostic(source);
+    }
+
+    [TestMethod]
+    public void DoesNotReportInvocationOfVoidMethod() {
+      const string source = @"
+using System.Threading.Tasks;
+
+class Test {
+  public void DoIt() {
+    DoIt2();
+  }
+
+  public void DoIt2() {}
+}";
+      VerifyDiagnostic(source);
+    }
+  }
+}
